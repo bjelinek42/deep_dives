1f0600bd32a1108400a4ad7e3fb14fd20076e2dc
ben <bjelinek42@gmail.com>
2022-03-15 17:44:46 -0600

    isbn X exception

--- isbn/isbn.rb
+++ isbn/isbn.rb
@@ -1,8 +1,10 @@
 def isbn_verify?(isbn_string)
   isbn_string = isbn_string.delete("-")
   if isbn_string.length == 10
-    if isbn_string = isbn_string.to_i
-      
+    if isbn_string == isbn_string.to_i
+      p "hello"
+    elsif isbn_string[-1] == "X"
+      p "goodbye" 
     else
       false
     end
