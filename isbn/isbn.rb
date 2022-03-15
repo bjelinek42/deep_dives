8b0c22cd6f6e2a983615858782789acd18f5fd20
ben <bjelinek42@gmail.com>
2022-03-15 17:40:21 -0600

    isbn length and integer conversion

--- isbn/isbn.rb
+++ isbn/isbn.rb
@@ -1,6 +1,14 @@
 def isbn_verify?(isbn_string)
   isbn_string = isbn_string.delete("-")
-  p isbn_string
+  if isbn_string.length == 10
+    if isbn_string = isbn_string.to_i
+      
+    else
+      false
+    end
+  else
+    false
+  end
 end
 
 puts isbn_verify?("168-05072-22")
