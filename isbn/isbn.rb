f5551d78b9393e295e42730a69a413825385d5ea
ben <bjelinek42@gmail.com>
2022-03-15 18:31:38 -0600

    isbn verify div 11

--- isbn/isbn.rb
+++ isbn/isbn.rb
@@ -1,14 +1,27 @@
 def isbn_verify?(isbn_string)
   isbn_string = isbn_string.delete("-")
   if isbn_string.length == 10
-    if isbn_string == isbn_string.to_i
-      p "hello"
+    p isbn_string, isbn_string.to_i
+    if isbn_string.count("a-zA-Z") == 0
+      i = 10
+      total = 0
+      isbn_string.each_char do |char|
+        total = total + (char.to_i * i)
+        i -= 1
+        p total
+      end
+      if total % 11 == 0
+        true
+      else
+        false
+      end
     elsif isbn_string[-1] == "X"
-      p "goodbye" 
     else
+      p 2
       false
     end
   else
+    p 1
     false
   end
 end
