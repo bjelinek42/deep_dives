87193e363e515e41c14664fb9ef067070f626337
ben <bjelinek42@gmail.com>
2022-03-15 18:50:03 -0600

    isbn working wet

--- isbn/isbn.rb
+++ isbn/isbn.rb
@@ -1,23 +1,36 @@
 def isbn_verify?(isbn_string)
   isbn_string = isbn_string.delete("-")
   if isbn_string.length == 10
-    p isbn_string, isbn_string.to_i
     if isbn_string.count("a-zA-Z") == 0
       i = 10
       total = 0
       isbn_string.each_char do |char|
         total = total + (char.to_i * i)
         i -= 1
-        p total
       end
       if total % 11 == 0
         true
       else
         false
       end
-    elsif isbn_string[-1] == "X"
+    elsif isbn_string[-1] == "X" && isbn_string.count("a-zA-WYZ") == 0
+      p "goodbye"
+      i = 10
+      total = 0
+      isbn_string.each_char do |char|
+        if char == "X"
+          total += 10
+        else
+          total = total + (char.to_i * i)
+          i -= 1
+        end
+      end
+      if total % 11 == 0
+        true
+      else
+        false
+      end
     else
-      p 2
       false
     end
   else
@@ -26,4 +39,4 @@ def isbn_verify?(isbn_string)
   end
 end
 
-puts isbn_verify?("168-05072-22")
+puts isbn_verify?("3-598-P1581-X")
